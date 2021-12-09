da528e303bc565c52f4fe610f304f1002bdc9e0b
Parth Patel <parth2002715@gmail.com>
2021-12-09 16:32:16 -0500

    Added some of the DB Context

--- WAR_UI/Program.cs
+++ WAR_UI/Program.cs
@@ -1,14 +1,20 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using WAR_UI;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
+var conn = "WARDbContext";
+builder.Services.AddDbContext<WARDbContext>(x => { x.UseSqlServer(builder.Configuration.GetConnectionString(conn)); });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
+app.UseCors(x => x.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+app.UseRouting();
 app.UseAuthorization();
+
 
 app.MapControllers();
 
---
+++ WAR_UI/WARDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WAR_UI
+{
+    public class WARDbContext : DbContext
+    {
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Cards> Cards { get; set; }
+        public DbSet<Game> Games { get; set; }
+         
+        public WARDbContext(DbContextOptions<WARDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Player>(p =>
+            {
+                p.ToTable("Players");
+                p.HasKey(p => p.Id);
+                p.Property(p => p.Name);
+                p.Property(p => p.Wins);
+            });
+        }
+    }
+}
